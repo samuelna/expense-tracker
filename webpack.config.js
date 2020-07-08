58c4a10eb292a205a0dc602a6663531a0f61d875
Samuel Na <samuelnasm@gmail.com>
2020-07-07 23:13:57 -0400

    Webpack config to add modules (images, css, etc)

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,36 @@ module.exports = {
       links: ['https://fonts.googleapis.com/css?family=Roboto']
     })
   ],
+  // module - loading css, images, fonts, and data
+  module: {
+    rules: [
+      // load css
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // load iamges
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader']
+      },
+      // load fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      // load data - csv, xml, tsv
+      // can load json without module
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loder']
+      }
+    ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
