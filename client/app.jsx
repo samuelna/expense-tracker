11c05a24c92a3ef2bd28b32c257981cffa8ba135
Samuel Na <samuelnasm@gmail.com>
2020-09-18 22:32:41 -0400

    Test material-ui button component

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,8 @@ export default class App extends React.Component {
       <div>
         <h1>hello this my react app component</h1>
         <p>wowwww</p>
+
+        <Button color="primary" variant="contained">Test button</Button>
       </div>
     );
   }
