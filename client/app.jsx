b84f4ec1b79bb4a656edbd712c219de4d96e3643
Samuel Na <samuelnasm@gmail.com>
2020-09-18 23:10:34 -0400

    Include CSS reset & meta tag

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,13 @@
 'use strict';
 
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>hello this my react app component</h1>
-        <p>wowwww</p>
-
-        <Button color="primary" variant="contained">Test button</Button>
+        <CssBaseline/>
       </div>
     );
   }
